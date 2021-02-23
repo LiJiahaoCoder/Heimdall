dffc16ce1cef14bcc50644ba0549aa19cde9c43b
Jiahao Li <lijiahao_shandong@163.com>
2021-02-23 23:12:49 +0800

    [feat] add header icons and add min-width to panel and sider

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { IconProvider, DEFAULT_ICON_CONFIGS } from "@icon-park/react";
 import Header from "@/components/header";
 import Sider from "@/components/sider";
 import Panel from "@/components/panel";
@@ -6,12 +7,17 @@ import Main from "@/components/main";
 import "./app.scss";
 
 const App = () => (
-  <>
+  <IconProvider
+    value={{
+      ...DEFAULT_ICON_CONFIGS,
+      size: 24,
+    }}
+  >
     <Header />
     <Sider />
     <Panel />
     <Main />
-  </>
+  </IconProvider>
 );
 
 export default App;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,16 @@
+import { Back, FullScreenOne } from "@icon-park/react";
+
 import styles from "./index.scss";
 
 const Header = () => (
   <section className={styles.header}>
+    <Back
+      className={styles.icon}
+      theme="filled"
+      fill="rgba(255, 255, 255, .9)"
+    />
     <h1 className={styles.title}>Heimdall</h1>
+    <FullScreenOne className={styles.icon} fill="rgba(255, 255, 255, .9)" />
   </section>
 );
 
