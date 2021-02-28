bad1e7c85f2bc9a3ad10bf3bf048ec56a14bd06b
Jiahao Li <lijiahao_shandong@163.com>
2021-02-28 23:04:41 +0800

    [feat] add close panel

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import { Close } from "@icon-park/react";
+import { ICON_COLOR } from "@/constants/styles";
+
 import styles from "./index.scss";
 
-const Panel = () => (
-  <section className={styles.panel}>
-    <h2 className={styles.title}>Panel</h2>
-  </section>
-);
+const Panel = () => {
+  const [visible, setVisible] = useState(true);
+
+  const onClosePanel = () => {
+    setVisible(false);
+  };
+
+  return (
+    <section
+      className={`${styles.panel} ${
+        visible ? styles.visible : styles.invisible
+      }`}
+    >
+      <Close className={styles.icon} fill={ICON_COLOR} onClick={onClosePanel} />
+      <div className={styles.content}>
+        <h2 className={styles.title}>Content</h2>
+      </div>
+    </section>
+  );
+};
 
 export default Panel;
